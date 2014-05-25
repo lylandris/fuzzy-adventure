2eefb0f3dcaf2b5e8065ded25ff49b65005f8bef
Lylandris <lylandris@github.com>
2014-05-25 15:44:58 +0800

    fix some errors

--- Process.cpp
+++ Process.cpp
@@ -1,5 +1,7 @@
 #include "Process.h"
 
+#include <iostream>
+
 using namespace ctc;
 
 Process::Process(void)
@@ -13,19 +15,31 @@ Process::~Process(void)
 
 void Process::Notify(Process& theProc, std::atomic<bool>& done)
 {
+#if 0
   while (!theProc._isToggle)
   {
     std::this_thread::yield();
   }
 
   theProc._isToggle = false;
+#endif
 
   theProc._currentTick++;
-  if (theProc._currentTick < theProc._triggerTick || theProc._isTaskDone)
+  if (theProc._currentTick < theProc._triggerTick)
   {
     done = true;
     return;
   }
+
+  if (theProc._isTaskDone)
+  {
+    theProc._isWaiting = false;
+    done = true;
+    return;
+  }
+
+  std::cout << "[" << std::this_thread::get_id() << "] ";
+  std::cout << "Notitied by main, theProc._currentTick=" << theProc._currentTick << std::endl;
 
   theProc._isFinished = false;
   theProc._isWaiting = false;
@@ -39,9 +53,11 @@ void Process::Notify(Process& theProc, std::atomic<bool>& done)
 
 void Process::WaitFor(Process& theProc, uint64_t latency)
 {
+  std::cout << "[" << std::this_thread::get_id() << "] ";
   theProc._isWaiting = true;
   theProc._triggerTick = theProc._currentTick + latency;
   theProc._isFinished = true;
+  std::cout << "Paused, next exec time is " << theProc._triggerTick << std::endl;
   while (theProc._isWaiting)
   {
     std::this_thread::yield();
@@ -55,5 +71,13 @@ uint64_t Process::GetTick(void)
 
 void Process::SetTaskDone(bool done)
 {
+  std::cout << "[" << std::this_thread::get_id() << "] ";
+  _isWaiting = true;
+  _isFinished = true;
+  std::cout << "The task is finished ..." << std::endl;
   _isTaskDone = done;
+  while (_isWaiting)
+  {
+    std::this_thread::yield();
+  }
 }
--- main.cpp
+++ main.cpp
@@ -13,10 +13,10 @@ std::vector<std::unique_ptr<PacketProcess>> packetList;
 std::vector<std::thread> eventList;
 std::vector<std::unique_ptr<std::atomic<bool>>> doneList;
 
-void PacketGenerator()
+static void PacketGenerator(void)
 {
   std::cout << "Start to send packets ..." << std::endl;
-  const uint64_t MAX_PACKET_ID = 4;
+  const uint64_t MAX_PACKET_ID = 100;
   const int32_t THE_PACKET_LEN_LIST_SIZE = 4;
   const size_t THE_PACKET_LEN[THE_PACKET_LEN_LIST_SIZE] = {64, 1518, 256, 9600};
   for (uint64_t id = 0; id < MAX_PACKET_ID; id++)
@@ -31,6 +31,7 @@ void PacketGenerator()
 
 int main(int argc, char** argv)
 {
+  PacketGenerator();
   const uint64_t totalTickStep = 1000;
 
   uint64_t globalTick = 0;
@@ -58,6 +59,8 @@ int main(int argc, char** argv)
       std::cerr << '.';
     }
   }
+
+  std::cout << "Almost finished ..." << std::endl;
 
   packetList.clear();
 
